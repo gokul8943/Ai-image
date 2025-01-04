0036a29b41ab0274799aa29effe2ae22d4679af7
Gokul M R <gokulmr331@gmail.com>
2025-01-04 11:24:10 +0530

    importingn error now fixed

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,10 @@
-import express from "express"
+import express, { Express } from "express";
 import cors from "cors"
 import dotenv from "dotenv"
+import { connectToDB } from "./config/db"
 
 dotenv.config()
-const app = express()
+const app: Express = express();
 const port: string = process.env.PORT || '5000';
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://localhost:5174'],
@@ -14,6 +15,8 @@ const corsOptions = {
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 app.use(cors(corsOptions))
+
+connectToDB()
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
