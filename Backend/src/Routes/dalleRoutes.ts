7d54e32fce85b15f292b7788cf155680030ca161
Gokul M R <gokulmr331@gmail.com>
2025-01-05 16:45:38 +0530

    remove commands

--- Backend/src/Routes/dalleRoutes.ts
+++ Backend/src/Routes/dalleRoutes.ts
@@ -3,7 +3,7 @@ import { dalleController } from '../Controllers/dalleController';
 
 const router = express.Router();
 
-// Use the specific method from the controller
+
 router.post('/', dalleController.generateImage);
 
 export default router;
