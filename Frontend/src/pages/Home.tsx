ab05ce98e44a248fdca0ed7c51a0eb64e533b679
Gokul M R <gokulmr331@gmail.com>
2025-01-06 09:01:54 +0530

    changes in home page

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 const Home = () => {
 
     const [loading, setLoading] = useState(false)
+    const [allPost,setAllPost] = useState(null)
 
     return (
         <div>
