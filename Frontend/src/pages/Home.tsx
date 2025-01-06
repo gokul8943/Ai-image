f3b5077f50c43b8831b8f637d7a691fbfc1aa3a3
Gokul M R <gokulmr331@gmail.com>
2025-01-06 08:46:55 +0530

    frontend component setup

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+
+const Home = () => {
+
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Home
